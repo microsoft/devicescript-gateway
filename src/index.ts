dbf88411db136467d3deccf573e4724e3b4f4263
pelikhan <jhalleux@microsoft.com>
2023-03-02 08:19:11 -0800

    logging

--- src/index.ts
+++ src/index.ts
@@ -118,6 +118,10 @@ async function main() {
         done()
     })
 
+    console.log({ SERVER_PORT: process.env["SERVER_PORT"] })
+    console.log({ WEBSITE_HOSTNAME: process.env["WEBSITE_HOSTNAME"] })
+    console.log({ WEBSITE_PLATFORM_VERSION: process.env["WEBSITE_PLATFORM_VERSION"]})
+
     await storage.setup()
     await initAuth(server)
     await wsskInit(server)
