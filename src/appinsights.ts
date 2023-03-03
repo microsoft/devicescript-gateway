14a65f2a4d04a3056b7087840fa4ce1bd05b72a8
pelikhan <jhalleux@microsoft.com>
2023-03-03 10:08:32 -0800

    sepate client for telemetry

--- src/appinsights.ts
+++ src/appinsights.ts
@@ -1,17 +1,33 @@
 import * as appInsights from "applicationinsights"
 import { ContextTagKeys } from "applicationinsights/out/Declarations/Contracts"
 
+let _devsTelemetry: appInsights.TelemetryClient
 export async function setup() {
-    appInsights.setup()
+    appInsights
+        .setup()
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true, true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true, false)
+        .setUseDiskRetryCaching(true)
+        .setAutoCollectPreAggregatedMetrics(true)
+        .setSendLiveMetrics(false)
+        .setAutoCollectHeartbeat(false)
+        .setAutoCollectIncomingRequestAzureFunctions(false)
+        .setInternalLogging(false, true)
+        .enableWebInstrumentation(false)
+        .start()
     // don't start, use custom events
-    const tel = appInsights.defaultClient
-    tel.trackEvent({
+    _devsTelemetry = new appInsights.TelemetryClient()
+    _devsTelemetry.trackEvent({
         name: "server.start",
     })
 }
 
 export function devsTelemetry(): appInsights.TelemetryClient {
-    return appInsights.defaultClient
+    return _devsTelemetry
 }
 
 export const contextTagKeys = new ContextTagKeys()
