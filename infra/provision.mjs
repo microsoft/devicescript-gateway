54169d9afc4157b33ccb1bd9a7b5415c148e63af
Michal Moskal <michal@moskal.me>
2023-03-03 16:11:26 -0800

    nicer location list

--- infra/provision.mjs
+++ infra/provision.mjs
@@ -25,9 +25,17 @@ if (!resourceGroup) throw "no resource group name given"
 let resourceLocation = process.env["DEVS_LOCATION"]
 if (!resourceLocation) {
     echo(chalk.blue("Searching for available Azure locations..."))
-    const locations = JSON.parse((await $`az account list-locations`).stdout).map(loc => loc.name)
-    echo(`locations: ${locations.join(", ")}`)
-    resourceLocation = await question(chalk.blue("Pick a region location for the resource group (env DEVS_LOCATION): "), { choices: locations })
+    const locations = JSON.parse(
+        (await $`az account list-locations`.quiet()).stdout
+    ).filter(l => !l.name.includes("stage"))
+    locations.sort((a, b) =>
+        a.regionalDisplayName < b.regionalDisplayName ? -1 : 1
+    )
+    for (const loc of locations)
+        echo(`${loc.name.padEnd(20)}  ${loc.regionalDisplayName}`)
+    resourceLocation = await question(chalk.blue("Pick a region location for the resource group (env DEVS_LOCATION): "), { 
+        choices: locations.map(l => l.name) 
+    })
 }
 if (!resourceLocation)
     throw "no location provided"
