1a8a4c51865c1ad40a1bb998c737ee1deaa7c150
pelikhan <jhalleux@microsoft.com>
2023-04-06 21:50:31 -0700

    support .env files

--- src/vault.ts
+++ src/vault.ts
@@ -1,9 +1,25 @@
 import { SecretClient } from "@azure/keyvault-secrets"
 import { DefaultAzureCredential } from "@azure/identity"
+import dotenv from "dotenv"
 
-export function createSecretClient() {
+export interface Secrets {
+    getSecret(name: string): Promise<{ value?: string }>
+}
+
+export function createSecretClient(): Secrets {
     const keyVaultName = process.env["DEVS_KEY_VAULT_NAME"]
-    if (!keyVaultName) throw new Error("DEVS_KEY_VAULT_NAME is empty")
+    if (!keyVaultName) {
+        console.log(
+            `no key DEVS_KEY_VAULT_NAME, using environment secrets or .env`
+        )
+        dotenv.config()
+        return {
+            async getSecret(name) {
+                const value = process.env[name]
+                return { value }
+            },
+        }
+    }
     const credential = new DefaultAzureCredential()
     const url = "https://" + keyVaultName + ".vault.azure.net"
     const client = new SecretClient(url, credential)
