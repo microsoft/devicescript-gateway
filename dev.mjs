1d9ac0bb2ee2e0c9157dd5bd26ce57d3d900b4bb
pelikhan <jhalleux@microsoft.com>
2023-04-07 11:46:13 -0700

    display docs

--- dev.mjs
+++ dev.mjs
@@ -1,5 +1,17 @@
 import dotenv from "dotenv"
 const azure = process.argv.includes("--azure")
+
 const out = dotenv.config({ path: azure ? "./.env" : "./local.env" })
 if (out.error) throw out.error
+
+if (!azure) {
+    console.log("Using local web server and Azurite")
+    console.log("- make sure to launch azurite with `yarn azurite`")
+    console.log(
+        `- Visual Studio Code connection string: ${process.env.DEVS_CONNECTION_STRING}`
+    )
+    console.log(
+        `- More documentation at https://microsoft.github.io/devicescript/developer/cloud/gateway`
+    )
+}
 import("./dist/src/index.js")
