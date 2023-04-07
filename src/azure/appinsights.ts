0e4fec9b5e11737e2c9da9523eb38fd379882096
pelikhan <jhalleux@microsoft.com>
2023-04-07 09:17:53 -0700

    error messages

--- src/azure/appinsights.ts
+++ src/azure/appinsights.ts
@@ -11,7 +11,7 @@ let _devsTelemetry: appInsights.TelemetryClient
 export async function setup() {
     const connString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING
     if (!connString) {
-        console.log(`no application insights connection string, skipping`)
+        console.log(`no env APPLICATIONINSIGHTS_CONNECTION_STRING, skipping`)
         return
     }
 
--- src/azure/keyvault.ts
+++ src/azure/keyvault.ts
@@ -6,7 +6,7 @@ export function createKeyVaultClient(): Secrets {
     const keyVaultName = process.env["DEVS_KEY_VAULT_NAME"]
     if (!keyVaultName) {
         console.log(
-            `no key DEVS_KEY_VAULT_NAME, using environment secrets or .env`
+            `no env DEVS_KEY_VAULT_NAME, using environment secrets and/or .env`
         )
         return undefined
     }
